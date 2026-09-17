f36f917e2e5b0f31da5aa8c9a778d8b88b823210
Uli6n6 <ulbgatova08@mail.ru>
2026-09-17 17:19:40 +0300

    17.09.5

---
+++ bin-counter.py
@@ -0,0 +1,27 @@
+import RPi.GPIO as GPIO
+import time
+def dec2bin(value):
+    return [int(element) for element in bin(value) [2:].zfill(8)]
+GPIO.setmode(GPIO.BCM)
+leds=[24,22,23,27,17,25,12,16]
+for led in leds:
+    GPIO.setup(led,GPIO.OUT)
+    GPIO.output(led,0)
+GPIO.setup(9,GPIO.IN)
+GPIO.setup(10,GPIO.IN)
+n=0
+while True:
+    GPIO.output(leds,dec2bin(n))
+    if GPIO.input(9)>0:
+        GPIO.output(leds[n%8],0)
+        n+=1
+        print(n,dec2bin(n))
+        GPIO.output(leds[n%8],1)
+        time.sleep(0.2)
+    if GPIO.input(10)>0:
+        GPIO.output(leds[n%8],0)
+        n-=1
+        print(n,dec2bin(n))
+        GPIO.output(leds[n%8],1)
+        time.sleep(0.2)
+    
